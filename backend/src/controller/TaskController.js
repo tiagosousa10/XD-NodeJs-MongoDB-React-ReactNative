bb8c0f3d13eb3d33730dcf47097e5b7da79e0296
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-11 18:26:28 +0100

    adicionando rotas para filtrar

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,16 @@
 const TaskModel = require('../model/TaskModel')
+const {
+  startOfDay,
+  endOfDay,
+  startOfWeek,
+  endOfWeek,
+  startOfMonth,
+  endOfMonth,
+  startOfYear,
+  endOfYear
+} = require('date-fns')
+
+const current = new Date()
 
 class TaskController {
   async create(req,res){
@@ -64,6 +76,84 @@ class TaskController {
         return res.status(500).json({'erro: ':error})
       })
   }
+
+  async late(req,res){
+    await TaskModel
+    .find({
+      'when': {'$lt' : current},
+      'macaddress': {'$in': req.body.macaddress}
+    })
+    .sort('when')
+    .then(response => {
+      return res.status(200).json(response)
+    })
+    .catch(error => {
+      return res.status(500).json(error)
+    })
+  }
+
+  async today(req,res){
+    await TaskModel
+    .find({
+      'macaddress': {'$in': req.body.macaddress},
+      'when' : {'$gte': startOfDay(current), '$lte': endOfDay(current)}
+    })
+    .sort('when')
+    .then(response => {
+      return res.status(200).json(response)
+    })
+    .catch(error => {
+      return res.status(500).json(error)
+    })
+  }
+
+  async week(req,res){
+    await TaskModel
+    .find({
+      'macaddress': {'$in': req.body.macaddress},
+      'when' : {'$gte': startOfWeek(current), '$lte': endOfWeek(current)}
+    })
+    .sort('when')
+    .then(response => {
+      return res.status(200).json(response)
+    })
+    .catch(error => {
+      return res.status(500).json(error)
+    })
+  }
+
+  async month(req,res){
+    await TaskModel
+    .find({
+      'macaddress': {'$in': req.body.macaddress},
+      'when' : {'$gte': startOfMonth(current), '$lte': endOfMonth(current)}
+    })
+    .sort('when')
+    .then(response => {
+      return res.status(200).json(response)
+    })
+    .catch(error => {
+      return res.status(500).json(error)
+    })
+  }
+
+  async year(req,res){
+    await TaskModel
+    .find({
+      'macaddress': {'$in': req.body.macaddress},
+      'when' : {'$gte': startOfYear(current), '$lte': endOfYear(current)}
+    })
+    .sort('when')
+    .then(response => {
+      return res.status(200).json(response)
+    })
+    .catch(error => {
+      return res.status(500).json(error)
+    })
+  }
+
+
+
 }
 
 
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -15,5 +15,11 @@ router.get('/:id', TaskController.show)
 router.delete('/:id', TaskController.delete)
 router.put('/:id/:done', TaskController.done)
 router.get('/filter/all', MacaddressValidation, TaskController.all)
+router.get('/filter/late', MacaddressValidation, TaskController.late) 
+router.get('/filter/today', MacaddressValidation, TaskController.today) 
+router.get('/filter/week', MacaddressValidation, TaskController.week) 
+router.get('/filter/month', MacaddressValidation, TaskController.month) 
+router.get('/filter/year', MacaddressValidation, TaskController.year) 
+
 
 module.exports = router
