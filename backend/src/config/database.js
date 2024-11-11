1819c3d4f4c6a193cce389307ddff456e1d6bc2d
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-11 16:41:57 +0100

    middleware para verificar campos da basedados

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 const url= 'mongodb://localhost:27017/todo'
-mongoose.connect(url, {useNewUrlParser:true})
+mongoose.connect(url)
 
 module.exports = mongoose;
---
+++ backend/src/middlewares/TaskValidation.js
@@ -0,0 +1,43 @@
+const TaskModel = require('../model/TaskModel')
+const { isPast } = require('date-fns');
+
+
+
+const TaskValidation = async (req,res,next) => {
+
+  const {macaddress,type,title,description,when} =  req.body;
+
+  if(!macaddress)
+     return res.status(400).json({error: 'MACaddress é obrigatorio'})
+  else if (!type)
+   return res.status(400).json({error:'Tipo é obrigatorio'})
+  else if (!title)
+   return res.status(400).json({error:'Titulo é obrigatorio'})
+  else if (!description)
+   return res.status(400).json({error:'Descricao é obrigatorio'})
+  else if (!when)
+   return res.status(400).json({error:'Data e Hora é obrigatorio'})
+  else if (isPast(new Date(when)))
+   return res.status(400).json({error:'Escolha uma data e hora FUTURA!'})
+  else {
+    let exists ;
+
+    exists = await TaskModel.findOne(
+      {
+        'when': {'$eq': new Date(when)},
+        'macaddress': {'$in': macaddress}
+      })
+
+      if(exists) {
+        return res.status(400).json({error:'Já existe uma TAREFA nesse Horario !'})
+
+      }
+
+    next()
+  }
+
+
+
+  }
+
+module.exports = TaskValidation;
