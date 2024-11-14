d0ce7976349550bfe9507e19ae0af45dd4054bbd
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-14 23:24:40 +0100

    botoes de filtros + css

--- todo/App.js
+++ todo/App.js
@@ -1,5 +1,7 @@
 import Home from './src/views/Home'
 
+//disabilita os alerts de erros do react-native - recomendado pelo professor
+console.disableYellowBox=true;
 
 export default function App() {
   return (
--- todo/src/views/Home/styles.js
+++ todo/src/views/Home/styles.js
@@ -6,6 +6,24 @@ const styles = StyleSheet.create({
     backgroundColor:'#fff',
     alignItems:'center', //alinhamento - HORIZONTAL
     justifyContent:'flex-start' //alinhamento - VERTICAL
+  },
+  filter:{
+    flexDirection:'row',
+    width:'100%',
+    justifyContent:'space-around',
+    height:70,
+    alignItems:'center'
+  },
+  filterTextActived:{
+    fontWeight:'bold',
+    fontSize:18,
+    color:'#ee6b26'
+  },
+  filterTextInatived:{
+    color:'#2029f5',
+    fontWeight:'bold',
+    fontSize:18,
+    opacity:0.5,
   }
 })
 
