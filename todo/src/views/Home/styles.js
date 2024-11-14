5081d54b8fb011618ad54b790aaf510a6d639284
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-14 21:25:50 +0100

    adicionando assets & Home page

---
+++ todo/src/views/Home/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center', //alinhamento - HORIZONTAL
+    justifyContent:'center' //alinhamento - VERTICAL
+  }
+})
+
+
+export default styles;
