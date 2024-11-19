c49712f6ef4b29d9232f859c5aba4e156d7ea8a1
tiagosousa <tiagosousa.tams@hotmail.com>
2024-11-19 16:50:33 +0100

    inputs + Text + Switch + css da pagina Task

--- todo/src/views/Task/index.js
+++ todo/src/views/Task/index.js
@@ -7,7 +7,8 @@ import {
   Text,
   TextInput,
   KeyboardAvoidingView,
-  TouchableOpacity
+  TouchableOpacity,
+  Switch
 } 
   from 'react-native'
 
@@ -17,18 +18,22 @@ import {
   import Header from '../../components/Header'
   import Footer from '../../components/Footer'
 
-
   import typeIcons from '../../utils/typeIcons.js'
 
+
   export default function Task(){
+    const [done,setDone] = useState(false)
+
+
     return(
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
         <Header showBack={true}  />
 
         <ScrollView style={{width:'100%'}}>
 
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{marginVertical:10}} >
             {typeIcons.map((icon) => (
+              icon !== null &&
               <TouchableOpacity >
               <Image source={icon} style={styles.imageIcon} />
               </TouchableOpacity>
@@ -42,10 +47,20 @@ import {
           <TextInput style={styles.input} maxLength={30} placeholder="Lembrar de fazer..." />
 
           <Text style={styles.label}>Detalhes</Text>
-          <TextInput style={styles.input} maxLength={200} placeholder="Detalhes da atividade..." multiline={true} />
+          <TextInput style={styles.inputarea} maxLength={200} placeholder="Detalhes da atividade..." multiline={true} />
 
+          <View style={styles.inLine}  >
+            <View style={styles.inputInline}>  
+              <Switch onValueChange={() => setDone(!done)} value={done} thumbColor={done? '#00761b' : '#ee6b26'} />
+              <Text style={styles.switchLabel}>Concluido</Text>
+            </View>
+            <TouchableOpacity>
+              <Text style={styles.removeLabel}>EXCLUIR</Text>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
 
+        <Footer icon={'save'}  />
       </KeyboardAvoidingView>
     )
   }
--- todo/src/views/Task/styles.js
+++ todo/src/views/Task/styles.js
@@ -3,7 +3,66 @@ import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
   container:{
-    
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'flex-start',
+    justifyContent:'flex-start'
+  },
+  imageIcon:{
+    width:40,
+    height:40,
+    marginHorizontal:10
+  },
+  label:{
+    color:'#707070',
+    fontSize:18,
+    paddingHorizontal:10,
+    marginTop:20,
+    marginBottom:5
+  },
+  input:{
+    fontSize:16,
+    padding:10,
+    width:'95%',
+    borderBottomColor:'#ee6b26',
+    borderWidth:1,
+    marginHorizontal:10
+  },
+  inputarea:{
+    fontSize:16,
+    padding:10,
+    width:'95%',
+    borderBottomColor:'#ee6b26',
+    borderWidth:1,
+    marginHorizontal:10,
+    borderRadius:10,
+    height:100,
+    textAlignVertical:'top'
+  },
+  inLine:{
+    width:'100%',
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between',
+    padding:10
+  },
+  inputInline:{
+    flexDirection:'row',
+    alignItems:'center',
+    marginVertical:10,
+  },
+  switchLabel:{
+    fontWeight:'bold',
+    color:'#ee6b26',
+    textTransform:'uppercase',
+    fontSize:16,
+    paddingLeft:10
+  },
+  removeLabel:{
+    fontWeight:'bold',
+    color:'#20295f',
+    textTransform:'uppercase',
+    fontSize:16,
   }
 })
 
